994a377177e4541f2ea29b0e404b1c089dbb6fb2
Barış ATALAY <barisatalay@teknasyon.com>
2022-01-18 15:16:55 +0300

    Added hardhat-gas-reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,9 @@
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-web3";
+import "hardhat-gas-reporter"
 
 require('dotenv').config()
-
+require("hardhat-gas-reporter");
 
 export default {
   solidity: {
@@ -13,5 +14,10 @@ export default {
         runs: 200
       }
     }
+  },
+  gasReporter: {
+    enabled: (process.env.REPORT_GAS) ? true : false,
+    currency: 'USD',
+    gasPrice: 21
   }
 }
